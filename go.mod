2e1938324329c22a332b4c8b46b5d9f97c86e3d1
Pedro Bueno Yerbes <pbuenoyerbes@cloudbees.com>
2026-08-17 13:20:42 +0200

    Dummy commit

---
+++ go.mod
@@ -0,0 +1,13 @@
+module test/dummy
+
+go 1.26.3
+
+require (
+	github.com/go-errors/errors v1.5.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/hashicorp/go-version v1.9.0 // indirect
+	github.com/rollout/rox-go/v6 v6.0.2 // indirect
+	github.com/rollout/sse v0.0.0-20181105093643-e422b54b3b28 // indirect
+	golang.org/x/net v0.57.0 // indirect
+	gopkg.in/cenkalti/backoff.v1 v1.1.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+// Import CloudBees Unify SDK
+import (
+	"fmt"
+
+	"github.com/rollout/rox-go/v6/server"
+)
+
+// Create Rox flags in the Flags container class
+type Flags struct {
+	EnableTutorial server.RoxFlag
+}
+
+var flags = &Flags{
+	// Define the feature flags
+	EnableTutorial: server.NewRoxFlag(false),
+}
+
+var rox *server.Rox
+
+func main() {
+
+	options := server.NewRoxOptions(server.RoxOptionsBuilder{})
+
+	rox := server.NewRox()
+
+	// Register the flags container with CloudBees Unify
+	rox.RegisterWithEmptyNamespace(flags)
+
+	// Setup the feature management environment key
+	<-rox.Setup("fc6699f7-ab42-4ed0-aadc-0654a006b10b", options)
+
+	// Boolean flag example
+	fmt.Printf("EnableTutorial's value is %t\n", flags.EnableTutorial.IsEnabled(nil))
+}
